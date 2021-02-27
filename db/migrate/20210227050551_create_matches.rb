e496f73f49ddafd17a7ba8a65c60e125e4ba2e5d
taishi9 <taishi0109@gmail.com>
2021-02-27 14:06:48 +0900

    matchmodel追加

---
+++ db/migrate/20210227050551_create_matches.rb
@@ -0,0 +1,10 @@
+class CreateMatches < ActiveRecord::Migration[6.0]
+  def change
+    create_table :matches do |t|
+      t.string :name
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
