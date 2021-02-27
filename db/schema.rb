5df1c92b09c52a2d272ee71e869ef550377049b9
taishi9 <taishi0109@gmail.com>
2021-02-27 14:42:38 +0900

    コンフリクト

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 2021_02_27_042559) do
 
   create_table "users", force: :cascade do |t|
