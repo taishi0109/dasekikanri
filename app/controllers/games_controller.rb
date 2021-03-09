3988082b674b371734ee4c846afd30829d8cb4ec
taishi9 <taishi0109@gmail.com>
2021-03-09 18:16:16 +0900

    controller作成、ルーティング修正？？

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,36 @@
 class GamesController < ApplicationController
+  def index
+    @games = Game.all
+    @game = Game.new
+  end
+
+  # def new
+  #   @games = Game.all(params[:id])
+  # end
+
   def create
     @game = Game.new(
-      name: params[:id]
+      name: game_params[:name],
+      user: game_params[:user]
     )
+    # if @game.save
+    # redirect_to games_path(@game.bat)
+    # @game = Game.new(params[:user_id])
+    # if @game.save
+    #   redirect_to games_create_path
+    # else
+    #   redirect_to games_path
+    # end
   end
 
   def show
-    @games = Game.all
+    @games = Game.new(user_id: params[:id])
+    # @newpost = Post.new(topic_id: params[:id])
+  end
+
+  private
+
+  def game_params
+    params[:game].permit(:id, :name, :user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
   get 'users/index'
-  resources :games, only: [:index, :create]
+  # get 'games/index'
+  # get 'games/create' => 'games#create'
+  # get 'games/show/:id' => 'games#show'
+  resources :games, only: [:index, :create, :show] do
+    get '/bats', to: 'bats#create'
+  end
 end
