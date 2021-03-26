e343ecea66ebda5019c951475cf0a83508388bf8
taishi9 <taishi0109@gmail.com>
2021-03-26 17:22:15 +0900

    結果一覧にトータル結果表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,151 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :games
+
+  def daritsu
+    hit = 0
+    dasekisu = 0
+    games.each do |game|
+      game.bats.each do |bat|
+        case bat.result
+        when '安打'
+          hit += 1
+          dasekisu += 1
+        when '二塁打'
+          hit += 1
+          dasekisu += 1
+        when '三塁打'
+          hit += 1
+          dasekisu += 1
+        when '本塁打'
+          hit += 1
+          dasekisu += 1
+        when '犠打・犠飛'
+          hit += 0
+          dasekisu += 0
+        when '四死球'
+          hit += 0
+          dasekisu += 0
+        when 'アウト'
+          hit += 0
+          dasekisu += 1
+        when '三振'
+          hit += 0
+          dasekisu += 1
+        when '併殺打'
+          hit += 0
+          dasekisu += 1
+        when '敵失'
+          hit += 0
+          dasekisu += 1
+        else
+          dasekisu += 1
+        end
+      end
+    end
+
+    if dasekisu == 0
+      0
+    else
+      hit.to_f / dasekisu
+    end
+  end
+
+  def dasuu
+    hit = 0
+    dasekisu = 0
+    games.each do |game|
+      game.bats.each do |bat|
+        case bat.result
+        when '安打'
+          hit += 1
+          dasekisu += 1
+        when '二塁打'
+          hit += 1
+          dasekisu += 1
+        when '三塁打'
+          hit += 1
+          dasekisu += 1
+        when '本塁打'
+          hit += 1
+          dasekisu += 1
+        when '犠打・犠飛'
+          hit += 0
+          dasekisu += 0
+        when '四死球'
+          hit += 0
+          dasekisu += 0
+        when 'アウト'
+          hit += 0
+          dasekisu += 1
+        when '三振'
+          hit += 0
+          dasekisu += 1
+        when '併殺打'
+          hit += 0
+          dasekisu += 1
+        when '敵失'
+          hit += 0
+          dasekisu += 1
+        else
+          dasekisu += 1
+        end
+      end
+    end
+
+    if dasekisu == 0
+      0
+    else
+      dasekisu
+    end
+  end
+
+  def anda
+    hit = 0
+    dasekisu = 0
+    games.each do |game|
+      game.bats.each do |bat|
+        case bat.result
+        when '安打'
+          hit += 1
+          dasekisu += 1
+        when '二塁打'
+          hit += 1
+          dasekisu += 1
+        when '三塁打'
+          hit += 1
+          dasekisu += 1
+        when '本塁打'
+          hit += 1
+          dasekisu += 1
+        when '犠打・犠飛'
+          hit += 0
+          dasekisu += 0
+        when '四死球'
+          hit += 0
+          dasekisu += 0
+        when 'アウト'
+          hit += 0
+          dasekisu += 1
+        when '三振'
+          hit += 0
+          dasekisu += 1
+        when '併殺打'
+          hit += 0
+          dasekisu += 1
+        when '敵失'
+          hit += 0
+          dasekisu += 1
+        else
+          dasekisu += 1
+        end
+      end
+    end
+
+    if dasekisu == 0
+      0
+    else
+      hit
+    end
+  end
 end
