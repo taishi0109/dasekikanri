1fff1de3286b6ac9fa49e3ea3574b111d55d41f8
taishi9 <taishi0109@gmail.com>
2021-03-27 10:36:36 +0900

    devise、ログイン前、リダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery whit: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:configure_permitted_parameters]
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
   end
